31dd6a7e19fb7ab790c20a81810c2b5526a25809
Azam Ahmed <aziskool20@gmail.com>
2022-04-05 16:52:13 -0400

    Add login and register

--- backend/app.js
+++ backend/app.js
@@ -2,12 +2,18 @@ const express = require('express');
 const logger = require('morgan');
 const router = express.Router();
 const cors = require('cors');
+const mongoose = require('mongoose')
+
+// Set up dotenv
+require('dotenv').config();
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 const authRouter = require('./routes/auth');
 
 const app = express();
+
+mongoose.connect(process.env.MONGO_URI);
 
 app.use(cors());
 app.use(logger('dev'));
