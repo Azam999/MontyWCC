e727164fc8833733e776be76f92cdb5d8ae4341c
Azam Ahmed <aziskool20@gmail.com>
2022-04-05 17:09:52 -0400

    Add register page

---
+++ frontend/pages/auth/register.jsx
@@ -0,0 +1,71 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+import { Button, Container, Form } from 'react-bootstrap';
+import { useRouter } from 'next/router';
+
+export default function RegisterPage() {
+    const [name, setName] = useState(null);
+    const [email, setEmail] = useState(null);
+    const [password, setPassword] = useState(null);
+
+    const router = useRouter();
+
+    async function handleRegister(e) {
+        e.preventDefault();
+        const res = await axios
+            .post('/api/auth/register', {
+                name: name,
+                email: email,
+                password: password,
+            })
+            .catch((err) => console.log(err.message));
+
+        if (res?.data.accessToken) {
+            localStorage.setItem('token', res.data.accessToken);
+            router.push('/');
+        }
+    }
+
+    return (
+        <Container>
+            <Button className='mt-3 mb-3' variant='primary' href='/'>
+                ❮
+            </Button>
+            <h1 className='display-4' style={{ fontWeight: 800 }}>
+                Register
+            </h1>
+            <Form>
+                <Form.Group className='mb-3' controlId='formBasicName'>
+                    <Form.Label>Name</Form.Label>
+                    <Form.Control
+                        placeholder='Enter name'
+                        onChange={(e) => setName(e.target.value)}
+                    />
+                </Form.Group>
+                <Form.Group className='mb-3' controlId='formBasicEmail'>
+                    <Form.Label>Email address</Form.Label>
+                    <Form.Control
+                        type='email'
+                        placeholder='Enter email'
+                        onChange={(e) => setEmail(e.target.value)}
+                    />
+                </Form.Group>
+                <Form.Group className='mb-3' controlId='formBasicPassword'>
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control
+                        type='password'
+                        placeholder='Password'
+                        onChange={(e) => setPassword(e.target.value)}
+                    />
+                </Form.Group>
+                <Button
+                    variant='primary'
+                    type='submit'
+                    onClick={(e) => handleRegister(e)}
+                >
+                    Register
+                </Button>
+            </Form>
+        </Container>
+    );
+}
