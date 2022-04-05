c0a6ac42dae904eb8bb24fbe136b09cbd4df8a3c
Azam Ahmed <aziskool20@gmail.com>
2022-04-04 20:23:26 -0400

    Setup backend

--- backend/app.js
+++ backend/app.js
@@ -1,16 +1,28 @@
 const express = require('express');
 const logger = require('morgan');
+const router = express.Router();
+const cors = require('cors');
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
+const authRouter = require('./routes/auth');
 
 const app = express();
 
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+router.use('/', indexRouter);
+router.use('/users', usersRouter);
+router.use('/auth', authRouter);
+
+app.use('/api', router);
+app.use('*', (req, res) => {
+    res.status(404).send({
+        message: 'Not Found',
+    });
+});
 
 module.exports = app;
